be1f259ed5b6cb659c4ee8c6aa3e6f8a5f37fc4c
Nevindu Shavihara <131888267+shavihara@users.noreply.github.com>
2025-04-24 22:53:29 +0530

    import Boostrap

--- home.php
+++ home.php
@@ -54,6 +54,7 @@ $conn->close();
 
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="home.ico">
     <title>home page</title>
     <link rel="stylesheet" href="home.css">
