746670b33e7fd16f492b3fed519e1032a3fd129f
Nevindu Shavihara <131888267+shavihara@users.noreply.github.com>
2025-05-29 03:23:11 +0530

    linking

--- Forget_password.php
+++ Forget_password.php
@@ -328,7 +328,7 @@
                 <p style="color: #666; margin: 20px 0;">
                     Your password has been reset successfully. You can now login with your new password.
                 </p>
-                <button onclick="window.location.href='login.html'" style="margin-top: 20px;">
+                <button onclick="window.location.href='home.php'" style="margin-top: 20px;">
                     Go to Login
                 </button>
             </div>
