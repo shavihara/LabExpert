5b2ca5defcc77c3fdc76ea2786999def4c45afdd
Nevindu Shavihara <131888267+shavihara@users.noreply.github.com>
2025-06-07 09:06:56 +0530

    new Frontend & Backend with py

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import { Routes, Route, Navigate } from 'react-router-dom'
+import Login from './components/Login'
+import Signup from './components/Signup'
+import ForgotPassword from './components/ForgotPassword'
+import Home from './components/Home'
+import Header from './components/Header'
+import './App.css'
+
+
+function App() {
+  return (
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
